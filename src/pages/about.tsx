4003000a98efc093f9cdaced5f81cddaf9771390
magnus <magnus@dahleide.com>
2022-09-12 11:42:18 +0200

    Added customer colors to tailwind config

---
+++ src/pages/about.tsx
@@ -0,0 +1,21 @@
+
+export default function About() {
+  return (
+    <div className="items-center h-screen mx-4 md:mx-auto md:w-1/5">
+      <div className="pt-10" />
+      <div className="px-2 md:px-5">
+        <a className="text-9xl">BABY</a>
+        <a>his</a>
+        <a>his</a>
+        {/* NAVBAR HERE */}
+      </div>
+
+      <main id="main" className="bg-magicpurple-400 text-magicpurple-100">
+        <h1>Test</h1>
+        <div className="pb-8" />
+      </main>
+
+      {/* fOOTER HERE */}
+    </div>
+  );
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        magicpurple: {
+          100: '#fbf5ff',
+          200: '#9075a6',
+          300: '#b26ee8',
+          400: '#251531',
+        },
+      },
+    },
   },
   plugins: [],
 };
